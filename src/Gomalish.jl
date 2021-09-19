92577d10ddd6e6fbd4c778fc1e56f1e5e5221f37
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-09-19 20:44:45 +0900

    add Gomalish.jl

---
+++ src/Gomalish.jl
@@ -0,0 +1,21 @@
+module Gomalish
+
+using Libdl
+using Pkg.Artifacts
+
+using CxxWrap
+
+const libgomalish = joinpath(@__DIR__, "..", "deps", "build", "lib", "libgomalish.$(dlext)")
+#const libgomalish = joinpath(artifact"libgomalish","libgomalish.$(Libdl.dlext)")
+isfile(libgomalish) && @wrapmodule(libgomalish, :define_julia_module)
+
+function do_something(N::Int)
+    v = vector(N, 1., 2.)
+    dot(v,v)
+end
+
+function __init__()
+    @initcxx
+end
+
+end # module
