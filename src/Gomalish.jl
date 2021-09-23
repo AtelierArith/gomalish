4c9b7746824ddcce66eef595dd075163c5a1046a
SatoshiTerasaki <s.terasaki@atelier-arith.jp>
2021-09-23 18:51:47 +0900

    rand

--- src/Gomalish.jl
+++ src/Gomalish.jl
@@ -10,14 +10,14 @@ const libgomalish = joinpath(@__DIR__, "..", "deps", "build", "lib", "libgomalis
 isfile(libgomalish) && @wrapmodule(libgomalish, :define_julia_module)
 
 export do_something, norm
-function do_something(N, value::Float64)
-    v = gomalish_vector{Float64}(N, value)
+function do_something(N, _min::Float64, _max::Float64)
+    v = gomalish_vector{Float64}(N, _min, _max)
     dot_f64(val(v), val(v))
 end
 
-function do_something(N, value::Float32)
-    v = gomalish_vector{Float32}(N, value)
-    dot_f32(val(v), val(v))
+function do_something(N, _min::Float32, _max::Float32)
+    v = gomalish_vector{Float32}(N, _min, _max)
+    dot_f64(val(v), val(v))
 end
 
 function norm(N, value::Float64)
