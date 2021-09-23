df7f6cd5936dcc13063b3d575f97ce65f21cb1d8
SatoshiTerasaki <s.terasaki@atelier-arith.jp>
2021-09-23 18:20:01 +0900

    save

--- deps/gomalish.cpp
+++ deps/gomalish.cpp
@@ -6,12 +6,12 @@
 using namespace jlcxx;
 
 
-double monolish_dot_double(monolish::vector<double> x, monolish::vector<double> y)
+double monolish_blas_dot_double(monolish::vector<double> x, monolish::vector<double> y)
 {
   return monolish::blas::dot(x, y);
 }
 
-float monolish_dot_float(monolish::vector<float> x, monolish::vector<float> y)
+float monolish_blas_dot_float(monolish::vector<float> x, monolish::vector<float> y)
 {
   return monolish::blas::dot(x, y);
 }
@@ -28,6 +28,19 @@ struct WrapMonolishVector_double
     wrapped.template constructor<const size_t, const double, const double>();
   }
 };
+
+struct WrapMonolishVector_float
+{
+  template<typename TypeWrapperT>
+  void operator()(TypeWrapperT&& wrapped)
+  {
+    typedef typename TypeWrapperT::type WrappedT;
+    wrapped.template constructor<const size_t>();
+    wrapped.template constructor<const size_t, const float>();
+    wrapped.template constructor<const size_t, const float, const float>();
+  }
+};
+
 
 template<typename T>
 struct gomalish_vector
@@ -51,30 +64,18 @@ struct WrapGomalishVector{
   }
 };
 
-struct WrapMonolishVector_float
-{
-  template<typename TypeWrapperT>
-  void operator()(TypeWrapperT&& wrapped)
-  {
-    typedef typename TypeWrapperT::type WrappedT;
-    wrapped.template constructor<const size_t>();
-    wrapped.template constructor<const size_t, const float>();
-    wrapped.template constructor<const size_t, const float, const float>();
-  }
-};
-
 template<typename T1> struct IsMirroredType<gomalish_vector<T1>> : std::false_type { };
 template<typename T1> struct IsMirroredType<monolish::vector<T1>> : std::false_type { };
 
 JLCXX_MODULE define_julia_module(Module &mod)
 {
-  mod.add_type<Parametric<TypeVar<1>>>("vector")
+  mod.add_type<Parametric<TypeVar<1>>>("each_type_vector")
      .apply<monolish::vector<double>>(WrapMonolishVector_double())
      .apply<monolish::vector<float>>(WrapMonolishVector_float());
   
-  mod.add_type<Parametric<TypeVar<1>>>("MyVector")
+  mod.add_type<Parametric<TypeVar<1>>>("vector")
      .apply<gomalish_vector<double>, gomalish_vector<float>>(WrapGomalishVector());
 
-  mod.method("dot", &monolish_dot_double);
-  mod.method("dot_float", &monolish_dot_float);
+  mod.method("dot_f64", &monolish_blas_dot_double);
+  mod.method("dot_f32", &monolish_blas_dot_float);
 }
--- src/Gomalish.jl
+++ src/Gomalish.jl
@@ -9,21 +9,20 @@ const libgomalish = joinpath(@__DIR__, "..", "deps", "build", "lib", "libgomalis
 # const libgomalish = joinpath(artifact"libgomalish","libgomalish.$(Libdl.dlext)")
 isfile(libgomalish) && @wrapmodule(libgomalish, :define_julia_module)
 
-export mydot
-function mydot(N, value::T) where T <: AbstractFloat
+export do_something, norm
+function do_something(N, value::T) where T <: AbstractFloat
     v = vector{T}(N, value)
     dot(v, v)
 end
 
-export My
-function My(N, value::Float64)
-    v = MyVector{Float64}(N, value)
-    dot(get_v(v), get_v(v))
+function norm(N, value::Float64)
+    v = vector{Float64}(N, value)
+    dot_f64(get_v(v), get_v(v))
 end
 
-function My(N, value::Float32)
-    v = MyVector{Float32}(N, value)
-    dot_float(get_v(v), get_v(v))
+function norm(N, value::Float32)
+    v = vector{Float32}(N, value)
+    dot_f32(get_v(v), get_v(v))
 end
 
 
