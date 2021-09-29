a300efbd53755a57653576b4ffafe9c415b8e72f
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-09-29 23:46:56 +0900

    wrap get_row/get_col

--- deps/monolish_matrix.cpp
+++ deps/monolish_matrix.cpp
@@ -10,6 +10,8 @@ struct WrapMonolishCOO_double
   {
     typedef typename TypeWrapperT::type WrappedT;
     wrapped.template constructor<const char*>();
+    wrapped.method("get_row", &WrappedT::get_row);
+    wrapped.method("get_col", &WrappedT::get_col);
   }
 };
 
@@ -20,14 +22,41 @@ struct WrapMonolishCOO_float
   {
     typedef typename TypeWrapperT::type WrappedT;
     wrapped.template constructor<const char*>();
+    wrapped.method("get_row", &WrappedT::get_row);
+    wrapped.method("get_col", &WrappedT::get_col);
+  }
+};
+
+struct WrapMonolishCRS_double
+{
+  template<typename TypeWrapperT>
+  void operator()(TypeWrapperT&& wrapped)
+  {
+    typedef typename TypeWrapperT::type WrappedT;
+    wrapped.template constructor<monolish::matrix::COO<double>>();
+  }
+};
+
+struct WrapMonolishCRS_float
+{
+  template<typename TypeWrapperT>
+  void operator()(TypeWrapperT&& wrapped)
+  {
+    typedef typename TypeWrapperT::type WrappedT;
+    wrapped.template constructor<monolish::matrix::COO<float>>();
   }
 };
 
 template<typename T> struct IsMirroredType<monolish::matrix::COO<T>> : std::false_type { };
+template<typename T> struct IsMirroredType<monolish::matrix::CRS<T>> : std::false_type { };
 
 void wrap_matrix(Module &mod)
 {
   mod.add_type<Parametric<TypeVar<1>>>("monolish_COO")
      .apply<monolish::matrix::COO<double>>(WrapMonolishCOO_double())
      .apply<monolish::matrix::COO<float>>(WrapMonolishCOO_float());
+
+  mod.add_type<Parametric<TypeVar<1>>>("monolish_CRS")
+     .apply<monolish::matrix::CRS<double>>(WrapMonolishCRS_double())
+     .apply<monolish::matrix::CRS<float>>(WrapMonolishCRS_float());
 }
