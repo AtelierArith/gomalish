3869706e6807a35d3db2544d8baaec86b63e6cb2
Satoshi Terasaki <terasakisatoshi.math@gmail.com>
2021-09-30 22:57:31 +0900

    use template

--- deps/monolish_equation.cpp
+++ deps/monolish_equation.cpp
@@ -4,7 +4,8 @@
 
 using namespace jlcxx;
 
-struct WrapMonolishCG_double
+template<typename PRECOND>
+struct WrapMonolishCG
 {
   template<typename TypeWrapperT>
   void operator()(TypeWrapperT&& wrapped)
@@ -15,48 +16,18 @@ struct WrapMonolishCG_double
     wrapped.method("set_tol", &WrappedT::set_tol);
     wrapped.method(
       "set_create_precond", 
-      [](WrappedT& w, monolish::equation::Jacobi<monolish::matrix::CRS<double>, double> &p){w.set_create_precond(p);}
+      [](WrappedT& w, PRECOND &p){w.set_create_precond(p);}
     );
     wrapped.method(
       "set_apply_precond", 
-      [](WrappedT& w, monolish::equation::Jacobi<monolish::matrix::CRS<double>, double> &p){w.set_apply_precond(p);}
+      [](WrappedT& w, PRECOND &p){w.set_apply_precond(p);}
     );
     wrapped.method("solve", &WrappedT::solve);
   }
 };
 
-struct WrapMonolishCG_float
-{
-  template<typename TypeWrapperT>
-  void operator()(TypeWrapperT&& wrapped)
-  {
-    typedef typename TypeWrapperT::type WrappedT;
-    wrapped.template constructor<>();
-    wrapped.method("set_maxiter", &WrappedT::set_maxiter);
-    wrapped.method("set_tol", &WrappedT::set_tol);
-    wrapped.method(
-      "set_create_precond", 
-      [](WrappedT& w, monolish::equation::Jacobi<monolish::matrix::CRS<float>, float> p){w.set_create_precond(p);}
-    );
-    wrapped.method(
-      "set_apply_precond", 
-      [](WrappedT& w, monolish::equation::Jacobi<monolish::matrix::CRS<float>, float> p){w.set_apply_precond(p);}
-    );
-    wrapped.method("solve", &WrappedT::solve);
-  }
-};
-
-struct WrapMonolishJacobi_double
-{
-  template<typename TypeWrapperT>
-  void operator()(TypeWrapperT&& wrapped)
-  {
-    typedef typename TypeWrapperT::type WrappedT;
-    wrapped.template constructor<>();
-  }
-};
-
-struct WrapMonolishJacobi_float
+template<typename MATRIX, typename Float>
+struct WrapMonolishJacobi
 {
   template<typename TypeWrapperT>
   void operator()(TypeWrapperT&& wrapped)
@@ -71,11 +42,11 @@ template<typename T1, typename T2> struct IsMirroredType<monolish::equation::Jac
 
 void wrap_equation(Module &mod){
   mod.add_type<Parametric<TypeVar<1>, TypeVar<2>>>("monolish_Jacobi")
-     .apply<monolish::equation::Jacobi<monolish::matrix::CRS<double>, double>>(WrapMonolishJacobi_double())
-     .apply<monolish::equation::Jacobi<monolish::matrix::CRS<float>, float>>(WrapMonolishJacobi_float());
+     .apply<monolish::equation::Jacobi<monolish::matrix::CRS<double>, double>>(WrapMonolishJacobi<monolish::matrix::CRS<double>, double>())
+     .apply<monolish::equation::Jacobi<monolish::matrix::CRS<float>, float>>(WrapMonolishJacobi<monolish::matrix::CRS<float>, float>());
 
   mod.add_type<Parametric<TypeVar<1>, TypeVar<2>>>("monolish_CG")
-     .apply<monolish::equation::CG<monolish::matrix::CRS<double>, double>>(WrapMonolishCG_double())
-     .apply<monolish::equation::CG<monolish::matrix::CRS<float>, float>>(WrapMonolishCG_float());
+     .apply<monolish::equation::CG<monolish::matrix::CRS<double>, double>>(WrapMonolishCG<monolish::equation::Jacobi<monolish::matrix::CRS<double>, double>>())
+     .apply<monolish::equation::CG<monolish::matrix::CRS<float>, float>>(WrapMonolishCG<monolish::equation::Jacobi<monolish::matrix::CRS<float>, float>>());
 
 }
