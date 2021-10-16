4b69fd0295a66d55331f76219a83cad946fd83bf
SatoshiTerasaki <s.terasaki@atelier-arith.jp>
2021-10-16 19:41:04 +0900

    wrap equations::...

--- deps/monolish_equation.cpp
+++ deps/monolish_equation.cpp
@@ -26,6 +26,16 @@ struct WrapMonolishCG
   }
 };
 
+struct WrapMonolishBiCGSTAB
+{
+  template<typename TypeWrapperT>
+  void operator()(TypeWrapperT&& wrapped)
+  {
+    typedef typename TypeWrapperT::type WrappedT;
+    wrapped.template constructor<>();
+  }
+};
+
 struct WrapMonolishJacobi
 {
   template<typename TypeWrapperT>
@@ -48,6 +58,7 @@ struct WrapMonolishnone
 
 template<typename T1, typename T2> struct IsMirroredType<monolish::equation::CG<T1, T2>> : std::false_type { };
 template<typename T1, typename T2> struct IsMirroredType<monolish::equation::Jacobi<T1, T2>> : std::false_type { };
+template<typename T1, typename T2> struct IsMirroredType<monolish::equation::BiCGSTAB<T1, T2>> : std::false_type { };
 template<typename T1, typename T2> struct IsMirroredType<monolish::equation::none<T1, T2>> : std::false_type { };
 
 void wrap_equation(Module &mod){
@@ -56,15 +67,18 @@ void wrap_equation(Module &mod){
      .apply<monolish::equation::Jacobi<monolish::matrix::CRS<float>, float>>(WrapMonolishJacobi());
 
   mod.add_type<Parametric<TypeVar<1>, TypeVar<2>>>("none")
-     .apply<monolish::equation::Jacobi<monolish::matrix::Dense<double>, double>>(WrapMonolishnone())
-     .apply<monolish::equation::Jacobi<monolish::matrix::Dense<float>, float>>(WrapMonolishnone());
+     .apply<monolish::equation::none<monolish::matrix::Dense<double>, double>>(WrapMonolishnone())
+     .apply<monolish::equation::none<monolish::matrix::Dense<float>, float>>(WrapMonolishnone());
 
   mod.add_type<Parametric<TypeVar<1>, TypeVar<2>>>("CG")
      .apply<monolish::equation::CG<monolish::matrix::CRS<double>, double>>(WrapMonolishCG<monolish::equation::Jacobi<monolish::matrix::CRS<double>, double>>())
      .apply<monolish::equation::CG<monolish::matrix::CRS<float>, float>>(WrapMonolishCG<monolish::equation::Jacobi<monolish::matrix::CRS<float>, float>>());
 
-/*  mod.add_type<Parametric<TypeVar<1>, TypeVar<2>>>("BiCGSTAB")
-     .apply<monolish::matrix::Dense<float>, float>(WrapMonolishBiCGSTAB<monolish::matrix::Dense<float>, float>())
-     .apply<monolish::matrix::Dense<double>, double>(WrapMonolishBiCGSTAB<monolish::matrix::Dense<double>, double>());
-     */
+  mod.add_type<Parametric<TypeVar<1>, TypeVar<2>>>("BiCGSTAB")
+     .apply<monolish::equation::BiCGSTAB<monolish::matrix::Dense<double>, double>>(WrapMonolishnone())
+     .apply<monolish::equation::BiCGSTAB<monolish::matrix::Dense<float>, float>>(WrapMonolishnone());
+
+  mod.add_type<Parametric<TypeVar<1>, TypeVar<2>>>("LU")
+     .apply<monolish::equation::LU<monolish::matrix::Dense<double>, double>>(WrapMonolishnone())
+     .apply<monolish::equation::LU<monolish::matrix::Dense<float>, float>>(WrapMonolishnone());
 }
