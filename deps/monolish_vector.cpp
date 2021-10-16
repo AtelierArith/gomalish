ef784c49761f2197a4babae21e409bbaeb0626b7
SatoshiTerasaki <s.terasaki@atelier-arith.jp>
2021-10-16 22:00:14 +0900

    del unused code

--- deps/monolish_vector.cpp
+++ deps/monolish_vector.cpp
@@ -24,30 +24,7 @@ struct WrapMonolishVector
     wrapped.module().unset_override_module();
   }
 };
-
-template<typename T>
-struct gomalish_vector
-{
-  typedef T type_value;
-  gomalish_vector(const size_t N): v(monolish::vector<T>(N)){}
-  gomalish_vector(const size_t N, const T value): v(monolish::vector<T>(N, value)){}
-  gomalish_vector(const size_t N, const T min, const T max): v(monolish::vector<T>(N, min, max)){}
-  monolish::vector<T> v;
-  monolish::vector<T> val(){return v;}
-};
   
-struct WrapGomalishVector{
-  template<typename TypeWrapperT>
-  void operator()(TypeWrapperT&& wrapped){
-    typedef typename TypeWrapperT::type WrappedT;
-    wrapped.template constructor<const size_t>();
-    wrapped.template constructor<const size_t, const typename WrappedT::type_value>();
-    wrapped.template constructor<const size_t, const typename WrappedT::type_value, const typename WrappedT::type_value>();
-    wrapped.method("val", &WrappedT::val);
-  }
-};
-
-template<typename T1> struct IsMirroredType<gomalish_vector<T1>> : std::false_type { };
 template<typename T1> struct IsMirroredType<monolish::vector<T1>> : std::false_type { };
 
 void wrap_vector(Module &mod)
@@ -56,7 +33,4 @@ void wrap_vector(Module &mod)
   mod.add_type<Parametric<TypeVar<1>>>("vector")
      .apply<monolish::vector<double>>(WrapMonolishVector<double>())
      .apply<monolish::vector<float>>(WrapMonolishVector<float>());
-
-  mod.add_type<Parametric<TypeVar<1>>>("gomalish_vector")
-     .apply<gomalish_vector<double>, gomalish_vector<float>>(WrapGomalishVector());
 }
