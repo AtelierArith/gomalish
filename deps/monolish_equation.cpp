70459a68e61ea14d7e429e8ed926b5c455e72cbc
SatoshiTerasaki <s.terasaki@atelier-arith.jp>
2021-10-16 17:48:55 +0900

    prepare to wrap solver

--- deps/monolish_equation.cpp
+++ deps/monolish_equation.cpp
@@ -36,16 +36,35 @@ struct WrapMonolishJacobi
   }
 };
 
+struct WrapMonolishnone
+{
+  template<typename TypeWrapperT>
+  void operator()(TypeWrapperT&& wrapped)
+  {
+    typedef typename TypeWrapperT::type WrappedT;
+    wrapped.template constructor<>();
+  }
+};
+
 template<typename T1, typename T2> struct IsMirroredType<monolish::equation::CG<T1, T2>> : std::false_type { };
 template<typename T1, typename T2> struct IsMirroredType<monolish::equation::Jacobi<T1, T2>> : std::false_type { };
+template<typename T1, typename T2> struct IsMirroredType<monolish::equation::none<T1, T2>> : std::false_type { };
 
 void wrap_equation(Module &mod){
   mod.add_type<Parametric<TypeVar<1>, TypeVar<2>>>("Jacobi")
      .apply<monolish::equation::Jacobi<monolish::matrix::CRS<double>, double>>(WrapMonolishJacobi())
      .apply<monolish::equation::Jacobi<monolish::matrix::CRS<float>, float>>(WrapMonolishJacobi());
 
+  mod.add_type<Parametric<TypeVar<1>, TypeVar<2>>>("none")
+     .apply<monolish::equation::Jacobi<monolish::matrix::Dense<double>, double>>(WrapMonolishnone())
+     .apply<monolish::equation::Jacobi<monolish::matrix::Dense<float>, float>>(WrapMonolishnone());
+
   mod.add_type<Parametric<TypeVar<1>, TypeVar<2>>>("CG")
      .apply<monolish::equation::CG<monolish::matrix::CRS<double>, double>>(WrapMonolishCG<monolish::equation::Jacobi<monolish::matrix::CRS<double>, double>>())
      .apply<monolish::equation::CG<monolish::matrix::CRS<float>, float>>(WrapMonolishCG<monolish::equation::Jacobi<monolish::matrix::CRS<float>, float>>());
 
+/*  mod.add_type<Parametric<TypeVar<1>, TypeVar<2>>>("BiCGSTAB")
+     .apply<monolish::matrix::Dense<float>, float>(WrapMonolishBiCGSTAB<monolish::matrix::Dense<float>, float>())
+     .apply<monolish::matrix::Dense<double>, double>(WrapMonolishBiCGSTAB<monolish::matrix::Dense<double>, double>());
+     */
 }
